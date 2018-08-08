f9bb81b9c3ed428cc8eeea321f9aa443c60446c7
Sul Hwa Lee <leesulh@kr.ibm.com>
2018-08-08 15:14:03 +0900

    jenkins test 1

--- app/identidock.py
+++ app/identidock.py
@@ -6,7 +6,7 @@ import hashlib
 app = Flask(__name__)
 # cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
-default_name = 'Joe Bloggs'
+default_name = 'Jenkins V1'
 
 @app.route('/', methods=['GET','POST'])
 def mainpage():
